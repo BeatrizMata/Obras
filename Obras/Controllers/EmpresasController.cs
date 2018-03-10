e08aedc0661be43227b0eaecf2b2cb82d0b0a739
Omar <Omar@LAPTOP-LKC8TS5S>
2018-03-10 01:19:34 -0600

    Final

---
+++ Obras/Controllers/EmpresasController.cs
@@ -0,0 +1,40 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Obras.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Obras.Controllers
+{
+    public class EmpresasController : Controller
+    {
+        private AppplicationDBContext db = new AppplicationDBContext();
+        // GET: Empresas
+        public ActionResult Index()
+        {
+
+            var model = db.dbEmpresa.ToList();
+            return View(model);
+        }
+        public ActionResult getEmpresas([DataSourceRequest] DataSourceRequest request)
+        {
+            IQueryable<Empresa> Details = db.dbEmpresa;
+            DataSourceResult result = Details.ToDataSourceResult(request, p => new Empresa
+            {
+                IdEmpresa = p.IdEmpresa,
+                //incluir el nombre de la tabla Usuarios
+                RegistroEdo = p.RegistroEdo,
+                RepresLegal = p.RepresLegal,
+                Tecnico = p.Tecnico,
+                RFC = p.RFC,
+                FechaRegistro = p.FechaRegistro
+
+            });
+            return Json(result, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
--- Obras/Models/AppplicationDBContext.cs
+++ Obras/Models/AppplicationDBContext.cs
@@ -19,12 +19,20 @@
 
         public DbSet <Obra> dbObra { get; set; }
         public DbSet <Usuarios> dbUsuarios { get; set; }
+        public DbSet<Empresa> dbEmpresa { get; set; }
 
-        // Agregue un DbSet para cada tipo de entidad que desee incluir en el modelo. Para obtener más información 
-        // sobre cómo configurar y usar un modelo Code First, vea http://go.microsoft.com/fwlink/?LinkId=390109.
+        public DbSet<Domicilio> dbDomicilio { get; set; }
+        public DbSet<Contacto> dbContacto { get; set; }
+        public DbSet<Especialidad> dbEspecialidad
+        {
+            get; set;
+        }
 
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
-    }
+            // Agregue un DbSet para cada tipo de entidad que desee incluir en el modelo. Para obtener más información 
+            // sobre cómo configurar y usar un modelo Code First, vea http://go.microsoft.com/fwlink/?LinkId=390109.
+
+            // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        }
 
     //public class MyEntity
     //{
